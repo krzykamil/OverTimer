82e560e738de971ebe296b5974960f4d0d2f6eb1
kkp <krzysiekkamilpiotrek@gmail.com>
2017-11-04 13:28:05 +0100

    edit show page

--- Overtime/db/seeds.rb
+++ Overtime/db/seeds.rb
@@ -1,8 +1,8 @@
-@user = User.create!(email: "JoeDoe@gmail.com", password: "Ilovemywife", password_confirmation: "Ilovemywife", first_name: "Joe", last_name: "Doe")
+@user = User.create!(email: "MisterUser@gmail.com", password: "Ilovemywife", password_confirmation: "Ilovemywife", first_name: "Joe", last_name: "Doe")
 
 puts "1 User created"
 
-AdminUser.create!(email: "admin@admin.com", password: "blablabla", password_confirmation: "blablabla", first_name: "Admin", last_name: "Doe")
+AdminUser.create!(email: "admin2@admin.com", password: "blablabla", password_confirmation: "blablabla", first_name: "Admin", last_name: "Doe")
 
 puts "1 AdminUser created"
 
