99be671739279589d578f07885a65ad54e7a1795
kkp <krzysiekkamilpiotrek@gmail.com>
2018-01-18 21:53:12 +0100

    Temp commit, fix capybara screenshot and then test

--- Overtime/spec/features/homepage_spec.rb
+++ Overtime/spec/features/homepage_spec.rb
@@ -1,20 +1,27 @@
 require 'rails_helper'
 
 describe 'Homepage' do
-  let!(:admin_user) { create :admin_user }
-  let!(:user) { create :user }
-  # let!(:other_user) { create :other_user }
-  let!(:post) { create :post, user_id: user.id }
-  # let!(:second_post) { create :second_post, user_id: user.id }
-  # let!(:third_post) { create :second_post, user_id: other_user.id }
+  let(:admin_user) { create :admin_user }
+  let(:user)       { create :user }
 
-  before do
-    login_as(admin_user, scope: :user)
+  context 'Admin homepage' do
+    before { login_as(admin_user, scope: :user) }
+    let!(:post) { create :post, user_id: user.id }
+    it 'allows admin to approve posts' do
+      visit root_path
+      click_on("approve_#{post.id}")
+      expect(post.reload.status).to eq 'approved'
+    end
   end
 
-  it 'allows to approve posts from homepage' do
-    visit root_path
-    click_on("approve_#{post.id}")
-    expect(post.reload.status).to eq 'approved'
+  context 'Employee homepage' do
+    before { login_as(user, scope: :user) }
+    let(:audit_log) { create :audit_log, user_id: user.id }
+    it 'allows employee to change audit logs status' do
+      login_as(user, scope: :user)
+      visit root_path
+      click_on("confirm_#{audit_log.id}")
+      expect().to eq ''
+    end
   end
 end
--- Overtime/spec/rails_helper.rb
+++ Overtime/spec/rails_helper.rb
@@ -5,12 +5,18 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'capybara/rspec'
+require 'capybara-screenshot/rspec'
 
 include Warden::Test::Helpers
 Warden.test_mode!
 ActiveRecord::Migration.maintain_test_schema!
+Capybara.asset_host = 'http://localhost:3000'
+Capybara::Screenshot.prune_strategy = :keep_last_run
+
 
 RSpec.configure do |config|
+  Capybara.asset_host = 'http://localhost:3000'
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
